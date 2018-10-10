b1f3a6e9c7fa5c1d0558fa76ea7446f48e50e4e1
mpowalisz <markpowalisz@gmail.com>
2018-10-10 15:28:15 -0400

    Updated logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,90 +4,84 @@ var btnProcured = 0;
 var wins = 0;
 var losses = 0;
 
-// reset function () {
+$(document).ready(function() {
+  // Assigns btnNeed a value between 19-100
+  var btnNeed = Math.floor(Math.random() * 120) + 19;
+  // Assigns each button a value from the array "value"
+  // ----------------Does not appear to be working properly-----------------
+  btn1 = value[Math.floor(Math.random() * 10) + 1];
+  btn2 = value[Math.floor(Math.random() * 10) + 1];
+  btn3 = value[Math.floor(Math.random() * 10) + 1];
+  btn4 = value[Math.floor(Math.random() * 10) + 1];
+  // Changes HTML to show btnNeed (Random)
+  $("#btnNeed").html(btnNeed);
 
-// }
+  // Changes HTML to show btnProcured (0)
+  $("#btnProcured").html(btnProcured);
 
-$(document).ready(function () {
-    // Assigns btnNeed a value between 19-100
-    var btnNeed = Math.floor(Math.random() * 120) + 19;
-    // Assigns each button a value from the array "value"
-    // ----------------Does not appear to be working properly-----------------
-    btn1 = value[Math.round(Math.random())];
-    btn2 = value[Math.round(Math.random())];
-    btn3 = value[Math.round(Math.random())];
-    btn4 = value[Math.round(Math.random())];
-    // Changes HTML to show btnNeed (Random)
+  // Button 1 function to update Buttons and call score function
+  $("#btn1").click(function() {
+    btnNeed = btnNeed - btn1;
+    btnProcured = btnProcured + btn1;
     $("#btnNeed").html(btnNeed);
-
-    // Changes HTML to show btnProcured (0)
     $("#btnProcured").html(btnProcured);
+    score();
+  });
 
-    // Button 1 function to update Buttons and call score function
-    $("#btn1").click(function () {
-        btnNeed = (btnNeed - btn1);
-        btnProcured = (btnProcured + btn1);
-        $("#btnNeed").html(btnNeed);
-        $("#btnProcured").html(btnProcured);
-        score();
-    })
+  // Button 2 function to update Buttons and call score function
+  $("#btn2").click(function() {
+    btnNeed = btnNeed - btn2;
+    btnProcured = btnProcured + btn2;
+    $("#btnNeed").html(btnNeed);
+    $("#btnProcured").html(btnProcured);
+    score();
+  });
 
-    // Button 2 function to update Buttons and call score function
-    $("#btn2").click(function () {
-        btnNeed = (btnNeed - btn2);
-        btnProcured = (btnProcured + btn2);
-        $("#btnNeed").html(btnNeed);
-        $("#btnProcured").html(btnProcured);
-        score();
-    })
+  // Button 3 function to update Buttons and call score function
+  $("#btn3").click(function() {
+    btnNeed = btnNeed - btn3;
+    btnProcured = btnProcured + btn3;
+    $("#btnNeed").html(btnNeed);
+    $("#btnProcured").html(btnProcured);
+    score();
+  });
+  // Button 4 function to update Buttons and call score function
+  $("#btn4").click(function() {
+    btnNeed = btnNeed - btn4;
+    btnProcured = btnProcured + btn4;
+    $("#btnNeed").html(btnNeed);
+    $("#btnProcured").html(btnProcured);
+    score();
+  });
 
-    // Button 3 function to update Buttons and call score function
-    $("#btn3").click(function () {
-        btnNeed = (btnNeed - btn3);
-        btnProcured = (btnProcured + btn3);
-        $("#btnNeed").html(btnNeed);
-        $("#btnProcured").html(btnProcured);
-        score();
-    })
-    // Button 4 function to update Buttons and call score function
-    $("#btn4").click(function () {
-        
-        btnNeed = (btnNeed - btn4);
-        btnProcured = (btnProcured + btn4);
-        $("#btnNeed").html(btnNeed);
-        $("#btnProcured").html(btnProcured);
-        score();
-    })
-
-    // Score Function
+  // Score Function
+  // Sets Condition for btnNeed < 0
+  function score() {
+    if (btnNeed < 0) {
+      alert("You're over budget");
+      losses++;
+      $("#losses").html("Losses: " + losses);
+      reset();
+    }
     // Sets Condition for btnNeed < 0
-    function score() {
-        if (btnNeed < 0) {
-            alert("You're over budget");
-            losses++;
-            $("#losses").html("Losses: " + losses);
-            reset();
-        }
-        // Sets Condition for btnNeed < 0
-        else if (btnNeed === 0) {
-            alert("You've won the war... this time");
-            wins++;
-            $("#wins").html("Wins: " + wins);
-            reset();
-        }
+    else if (btnNeed === 0) {
+      alert("You've won the war... this time");
+      wins++;
+      $("#wins").html("Wins: " + wins);
+      reset();
     }
-    // End Score Function
+  }
+  // End Score Function
 
-    // Reset Function (called by Score Function)
-    function reset() {
-        btnNeed = Math.floor(Math.random() * 120) + 19;
-        $("#btnNeed").html(btnNeed);
-        btnProcured = 0;
-        $("#btnProcured").html(btnProcured);
-        btn1 = value[Math.round(Math.random())];
-        btn2 = value[Math.round(Math.random())];
-        btn3 = value[Math.round(Math.random())];
-        btn4 = value[Math.round(Math.random())];
-    }
-
-})
+  // Reset Function (called by Score Function)
+  function reset() {
+    btnNeed = Math.floor(Math.random() * 120) + 19;
+    $("#btnNeed").html(btnNeed);
+    btnProcured = 0;
+    $("#btnProcured").html(btnProcured);
+    btn1 = value[Math.floor(Math.random() * 10) + 1];
+    btn2 = value[Math.floor(Math.random() * 10) + 1];
+    btn3 = value[Math.floor(Math.random() * 10) + 1];
+    btn4 = value[Math.floor(Math.random() * 10) + 1];
+  }
+});
